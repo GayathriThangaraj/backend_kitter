c1596c630be32f13963d57b971e6f7eb9f4bd22a
GayathriThangaraj <125265735+GayathriThangaraj@users.noreply.github.com>
2024-04-03 15:01:28 +0530

    Delete Pet_Shop/pet_shop directory

--- Pet_Shop/pet_shop/src/main/java/com/example/pet_shop/repository/ShopRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.pet_shop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.pet_shop.model.Shop;
-
-public interface ShopRepo extends JpaRepository<Shop,String>{
-    
-}
